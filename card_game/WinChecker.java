1e767945e5b6086aa90bf94af529466922792bfb
Beth Fraser <bethanyfraser@live.co.uk>
2016-07-28 16:58:44 +0100

    initial commit, win checker working

---
+++ card_game/WinChecker.java
@@ -0,0 +1,226 @@
+package card_game;
+
+import card_game.*;
+import java.util.*;
+
+public class WinChecker {
+
+  private Player player1;
+  private Player player2;
+  private ArrayList<Player> winners = new ArrayList<Player>();
+
+  public void setPlayers(Player player1, Player player2){
+    this.player1 = player1;
+    this.player2 = player2;
+  }
+
+  public Player checkForWin(){
+
+    checkForPrial(player1);
+    checkForPrial(player2);
+
+    if (winners.size() > 0) {
+      if (winners.size() == 1) {
+        return winners.get(0);
+      }
+      else {
+        // If both players have a Prial, the player whose Prial is of the highest number wins. This version has Ace as low. Could add in to have 3 as the top number. 
+        if (player1.getHand()[0].getNumber() > player2.getHand()[0].getNumber()){
+          return player1;
+        }
+        return player2;
+      }
+    }
+
+    checkForRunningFlush(player1);
+    checkForRunningFlush(player2);
+
+    if(winners.size() > 0){
+      if (winners.size() == 1){
+        return winners.get(0);
+      }
+      else {
+        // If both players have a running flush, the player with the highest run (shown by the highest card of the run) wins. If both runs are the same, player who refused to bet higher wins (if no betting, just first player by random luck).
+        return highestCard(player1, player2);
+      }
+    }
+
+    checkForRun(player1);
+    checkForRun(player2);
+
+    if(winners.size() > 0){
+      if (winners.size() == 1){
+        return winners.get(0);
+      }
+      else {
+        // If both players have a standard run (no flush), the one with the highest card in wins. If both runs are the same, player who refused to bet higher wins (if no betting, just first player by random luck).
+        return highestCard(player1, player2);
+      }
+    }
+
+    checkForFlush(player1);
+    checkForFlush(player2);
+
+    if (winners.size() > 0) {
+      if (winners.size() == 1) {
+        return winners.get(0);
+      }
+      else {
+        return highestCardAll(player1, player2);
+      }
+    }
+
+    checkForPair(player1);
+    checkForPair(player2);
+
+    if (winners.size() > 0) {
+      if (winners.size() == 1) {
+        return winners.get(0);
+      }
+      else {
+        return highestPair(player1, player2);
+      }
+    }
+
+    return highestCardAll(player1, player2);
+  }
+
+
+  public boolean checkForPrial(Player player){
+    Card[] hand = player.getHand();
+
+    if (hand[0].getNumber() == hand[1].getNumber() && hand[1].getNumber() == hand[2].getNumber()){
+      winners.add(player);
+      return true;
+    }
+    return false;
+  }
+
+  public boolean checkForFlush(Player player){
+    Card[] hand = player.getHand();
+
+    if(hand[0].getSuit() == hand[1].getSuit() && hand[1].getSuit() == hand[2].getSuit()){
+      winners.add(player);
+      return true;
+    }
+    return false;
+  }
+
+  public boolean checkForRun(Player player){
+    Card[] hand = player.getHand();
+    ArrayList<Integer> cardNumbers = makeCardNumbersArray(hand);
+    int max = findMaxCard(hand);
+
+    if (cardNumbers.contains(max - 1)){
+      if (cardNumbers.contains(max - 2)){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean checkForRunningFlush(Player player){
+    if(checkForFlush(player) && checkForRun(player)){
+      return true;
+    }
+    return false;
+  }
+
+  public int findMaxCard(Card[] hand){
+    ArrayList<Integer> cardNumbers = makeCardNumbersArray(hand);
+    int max = (int) Collections.max(cardNumbers);
+    return max;
+  }
+
+  public ArrayList<Integer> makeCardNumbersArray(Card[] hand){
+    ArrayList<Integer> cardNumbers = new ArrayList<Integer>();
+    cardNumbers.add(hand[0].getNumber());
+    cardNumbers.add(hand[1].getNumber());
+    cardNumbers.add(hand[2].getNumber());
+    return cardNumbers;
+  }
+
+  public Player highestCard(Player player1, Player player2){
+    int player1Max = findMaxCard(player1.getHand());
+    int player2Max = findMaxCard(player2.getHand());
+
+    if (player1Max > player2Max){
+      return player1;
+    }
+    else if (player2Max > player1Max){
+      return player2;
+    }
+    // if there is no diff in highest card of running flush, return player who refused to bet any higher - always enter this player as player1. 
+    return player1;
+  }
+
+  public Player highestCardAll(Player player1, Player player2){
+
+    ArrayList<Integer> player1Numbers = makeCardNumbersArray(player1.getHand());
+    ArrayList<Integer> player2Numbers = makeCardNumbersArray(player2.getHand());
+
+    Collections.sort(player1Numbers);
+    Collections.sort(player2Numbers);
+
+    for (int i = 2; i > -1; i--){
+      if (player1Numbers.get(i) > player2Numbers.get(i)){
+        return player1;
+      }
+      else if (player2Numbers.get(i) > player1Numbers.get(i)){
+        return player2;
+      }
+    }
+
+    return player1;
+  }
+
+  public boolean checkForPair(Player player){
+    Card[] hand = player.getHand();
+
+    if (hand[0].getNumber() == hand[1].getNumber() || hand[1].getNumber() == hand[2].getNumber() || hand[0].getNumber() == hand[2].getNumber()){
+      winners.add(player);
+      return true;
+    }
+    return false;
+  }
+
+  public Player highestPair(Player player1, Player player2){
+    // working on it
+
+    // get the pair
+    ArrayList<Integer> player1Numbers = makeCardNumbersArray(player1.getHand());
+    ArrayList<Integer> player2Numbers = makeCardNumbersArray(player2.getHand());
+
+    Collections.sort(player1Numbers);
+    Collections.sort(player2Numbers);
+
+    // When 3 cards including a pair are sorted, the middle card is always going to be part of the pair. 
+
+    Integer player1Pair = player1Numbers.get(1);
+    player1Numbers.remove(player1Pair);
+    player1Numbers.remove(player1Pair);
+    Integer player1Spare = player1Numbers.get(0);
+
+    Integer player2Pair = player2Numbers.get(1);
+    player2Numbers.remove(player2Pair);
+    player2Numbers.remove(player2Pair);
+    Integer player2Spare = player2Numbers.get(0);
+
+    // compare pair cards
+    if (player1Pair > player2Pair){
+      return player1;
+    }
+    else if (player2Pair > player1Pair){
+      return player2;
+    }
+    else if (player2Spare > player1Spare){
+      return player2;
+    }
+    return player1;
+  }
+
+  public String printHands(){
+    return player1.printHand() + " -- " + player2.printHand();
+  }
+
+}
---
+++ card_tests/WinCheckerTest.java
@@ -0,0 +1,176 @@
+import card_game.*;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+public class WinCheckerTest {
+
+  Player player1;
+  Player player2;
+  WinChecker winChecker;
+  Card card1;
+  Card card2;
+  Card card3;
+  Card card4;
+  Card card5;
+  Card card6;
+  Card card7;
+  Card card8;
+  Card card9;
+
+  @Before
+  public void before(){
+    player1 = new Player("Beth");
+    player2 = new Player("Rick");
+
+    card1 = new Card(2, Suit.Clubs);
+    card2 = new Card(3, Suit.Clubs);
+    card3 = new Card(4, Suit.Clubs);
+
+    card4 = new Card(3, Suit.Clubs);
+    card5 = new Card(3, Suit.Spades);
+    card6 = new Card(3, Suit.Diamonds);
+
+    card7 = new Card(6, Suit.Clubs);
+    card8 = new Card(6, Suit.Spades);
+    card9 = new Card(6, Suit.Hearts);
+
+    player1.setHand(card1, card2, card3);
+    player2.setHand(card4, card5, card6);
+    winChecker = new WinChecker();
+    winChecker.setPlayers(player1, player2);
+  }
+
+  @Test
+  public void hasTwoHands(){
+    assertEquals("Two of Clubs, Three of Clubs, Four of Clubs -- Three of Clubs, Three of Spades, Three of Diamonds", winChecker.printHands());
+  }
+
+  @Test
+  public void canRecognisePrial(){
+    assertEquals(true, winChecker.checkForPrial(player2));
+  }
+
+  @Test
+  public void canRecogniseNoPrial(){
+    assertEquals(false, winChecker.checkForPrial(player1));
+  }
+  
+  @Test
+  public void canRecogniseFlush(){
+    assertEquals(true, winChecker.checkForFlush(player1));
+  }
+
+  @Test
+  public void canRecogniseNoFlush(){
+    assertEquals(false, winChecker.checkForFlush(player2));
+  }
+
+  @Test 
+  public void canRecogniseRun(){
+    assertEquals(true, winChecker.checkForRun(player1));
+  }
+
+  @Test 
+  public void canRecogniseNoRun(){
+    assertEquals(false, winChecker.checkForRun(player2));
+  }
+
+  @Test
+  public void canRecogniseRunningFlush(){
+    assertEquals(true, winChecker.checkForRunningFlush(player1));
+  }
+
+  @Test
+  public void canRecogniseNoRunningFlush(){
+    player2.setHand(card1, card3, card6);
+    winChecker.setPlayers(player1, player2);
+    assertEquals(false, winChecker.checkForRunningFlush(player2));
+  }
+
+  @Test
+  public void canFindMaxCard(){
+    assertEquals(4, winChecker.findMaxCard(player1.getHand()));
+  }
+
+  @Test
+  public void canMakeNumbersArray(){
+    ArrayList<Integer> testArray = new ArrayList<Integer>();
+    testArray.add(2);
+    testArray.add(3);
+    testArray.add(4);
+    assertEquals(testArray, winChecker.makeCardNumbersArray(player1.getHand()));
+  }
+
+  @Test
+  public void canFindHighestSingleCard(){
+    assertEquals(player1, winChecker.highestCard(player1, player2));
+  } 
+
+  @Test
+  public void canFindHighestMultipleCards(){
+    player2.setHand(card4, card5, card3);
+    winChecker.setPlayers(player1, player2);
+    assertEquals(player2, winChecker.highestCardAll(player1, player2));
+  }
+
+  @Test
+  public void canRecognisePair(){
+    player2.setHand(card5, card6, card8);
+    winChecker.setPlayers(player1, player2);
+    assertEquals(true, winChecker.checkForPair(player2));
+  }
+
+  @Test
+  public void canRecogniseNoPair(){
+    assertEquals(false, winChecker.checkForPair(player1));
+  }
+
+  @Test
+  public void canRecogniseHighestPair(){
+    player2.setHand(card8, card9, card1);
+    player1.setHand(card5, card6, card8);
+    winChecker.setPlayers(player1, player2);
+    assertEquals(player2, winChecker.highestPair(player1, player2));
+  }
+
+  @Test
+  public void canRecogniseHighestSpare(){
+    player2.setHand(card8, card9, card2);
+    player1.setHand(card8, card9, card1);
+    winChecker.setPlayers(player1, player2);
+    assertEquals(player2, winChecker.highestPair(player1, player2));
+  }
+
+  @Test
+  public void canCheckWinner_OnePrial(){
+    assertEquals(player2, winChecker.checkForWin());
+  }
+
+  @Test
+  public void canCheckWinner_TwoPrials(){
+    player1.setHand(card7, card8, card9);
+    winChecker.setPlayers(player1, player2);
+    assertEquals(player1, winChecker.checkForWin());
+  }
+
+  @Test
+  public void canCheckWinner_OneRunningFlush(){
+    player2.setHand(card4, card7, card8);
+    winChecker.setPlayers(player1, player2);
+    assertEquals(player1, winChecker.checkForWin());
+  }
+
+  @Test
+  public void canCheckWinner_TwoRunningFlushes(){
+    Card cardA = new Card(3, Suit.Hearts);
+    Card cardB = new Card(4, Suit.Hearts);
+    Card cardC = new Card(5, Suit.Hearts);
+
+    player2.setHand(cardA, cardB, cardC);
+    winChecker.setPlayers(player1, player2);
+    assertEquals(player2, winChecker.checkForWin());
+  }
+
+}
